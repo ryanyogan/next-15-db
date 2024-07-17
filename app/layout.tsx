7d1972ef12a5c7ccbd0a7cbec2473c4f50a86d23
Ryan Yogan <ryanyogan@gmail.com>
2024-07-17 14:00:17 -0500

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import "@/app/ui/global.css";
+import { Metadata } from "next";
 import { inter } from "./ui/fonts";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "Acme Dashboard",
+  metadataBase: new URL("https://next-15-db.vercel.app"),
+};
 
 export default function RootLayout({
   children,
