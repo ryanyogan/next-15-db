bd392531c463d3202f60912ca7ec0842f9d1d99e
Ryan Yogan <ryanyogan@gmail.com>
2024-07-16 23:02:15 -0500

    errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,10 +26,14 @@ export async function createInvoice(_: any, formData: FormData) {
   let amountInCents = amount * 100;
   let date = new Date().toISOString().split("T")[0];
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (error) {
+    return { message: "Database Error: Failed to Update Invoice." };
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -44,17 +48,29 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   let amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch (error) {
+    return {
+      message: "Database error",
+    };
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath("/dashboard/invoices");
+  throw new Error("Not implemented");
+  // try {
+  //   await sql`DELETE FROM invoices WHERE id = ${id}`;
+  //   revalidatePath("/dashboard/invoices");
+  //   return { message: "Invoice Deleted" };
+  // } catch (error) {
+  //   return { message: "Database Error: Failed to Update Invoice." };
+  // }
 }
